785589cc1de481f125d4c3cfd67c2d97df67a3f9
xuan <xuancbblin@gmail.com>
2022-10-11 17:18:19 +0800

    chore : 新增 unplugin-vue-components

--- vite.config.js
+++ vite.config.js
@@ -3,10 +3,11 @@ import { fileURLToPath, URL } from 'node:url';
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import WindiCSS from 'vite-plugin-windicss';
+import Components from 'unplugin-vue-components/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), WindiCSS()],
+  plugins: [vue(), WindiCSS(), Components()],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
