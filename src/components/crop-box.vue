906f1dd01f5dc44126002e5c14e46b31eaf82891
xuan <xuancbblin@gmail.com>
2022-10-12 19:04:36 +0800

    feat : 測試 v-model

--- src/components/crop-box.vue
+++ src/components/crop-box.vue
@@ -6,7 +6,7 @@
     :stencil-component="CircleStencil"
   />
 
-  <slide-bar />
+  <slide-bar v-model:slideValue.number="slideValue" />
 
   <div class="flex mb-5 justify-center">
     <button class="bg-dark-700 text-white py-2 px-2" @click="checkCrop">確定</button>
@@ -14,7 +14,7 @@
 </template>
 
 <script>
-import { ref } from 'vue';
+import { ref, watch } from 'vue';
 import { Cropper, CircleStencil } from 'vue-advanced-cropper';
 import 'vue-advanced-cropper/dist/style.css';
 
@@ -30,16 +30,22 @@ export default {
   emits: ['getCrop'],
   setup(props, { emit }) {
     const cropperBox = ref(null);
+    const slideValue = ref(0);
 
     const checkCrop = () => {
       const { canvas } = cropperBox.value.getResult();
       emit('getCrop', canvas.toDataURL());
     };
 
+    watch(slideValue, newSlideValue => {
+      console.log(newSlideValue);
+    });
+
     return {
       CircleStencil,
       cropperBox,
       checkCrop,
+      slideValue,
     };
   },
 };
--- src/components/slide-bar.vue
+++ src/components/slide-bar.vue
@@ -1,13 +1,23 @@
 <template>
   <div class="flex mb-4 items-center justify-center">
     <i-material-symbols-zoom-out-rounded />
-    <input type="range" min="0" max="100" class="h-8 w-80" />
+    <input
+      type="range"
+      min="0"
+      max="100"
+      class="h-8 w-80"
+      :value="slideValue"
+      @input="$emit('update:slideValue', $event.target.value)"
+    />
     <i-material-symbols-zoom-in-rounded />
   </div>
 </template>
 
 <script>
 export default {
+  props: {
+    slideValue: Number,
+  },
   setup() {},
 };
 </script>
