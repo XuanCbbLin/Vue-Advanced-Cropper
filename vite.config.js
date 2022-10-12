82fc55e38ca466f68b6f1b34d975083fae2770b8
xuan <xuancbblin@gmail.com>
2022-10-12 11:58:45 +0800

    chore : 新增 unplugin-icons

--- vite.config.js
+++ vite.config.js
@@ -4,10 +4,19 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import WindiCSS from 'vite-plugin-windicss';
 import Components from 'unplugin-vue-components/vite';
+import Icons from 'unplugin-icons/vite';
+import IconsResolver from 'unplugin-icons/resolver';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue(), WindiCSS(), Components()],
+  plugins: [
+    vue(),
+    WindiCSS(),
+    Components({
+      resolvers: IconsResolver(),
+    }),
+    Icons(),
+  ],
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url)),
